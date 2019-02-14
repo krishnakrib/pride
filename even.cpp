ff941fb972f89b5388b9dddaa84359efd15f3d93
krishnakrib <42169952+krishnakrib@users.noreply.github.com>
2019-02-14 10:27:32 +0530

    Create even.cpp

---
+++ even.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number;
+printf("enter the number:");
+scanf("%d",&number);
+if(num/2=0)
+printf("%d is even");
+else
+printf("%d is odd");
+getch()
+}
